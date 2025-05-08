184496447b25d0dfa5ac066253b151ccd17daee5
SibFox <mr.dastin02@yandex.ru>
2025-05-08 17:45:33 +0300

    Добавьте файлы проекта.

---
+++ DoubleMinimal/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.Numerics;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Введите несколько целых чисел.\nЧтобы продолжить, введите любое не число.");
+
+        LinkedList<int> nums = [];
+        while (true)
+        {
+            try
+            {
+                nums.AddLast(int.Parse(Console.ReadLine()));
+            }
+            catch
+            {
+                break;
+            }
+        }
+
+        FindMinimalsSummirize([.. nums]);
+    }
+
+    public static int FindMinimalsSummirize(int[]? numeros = null)
+    {
+        ArgumentNullException.ThrowIfNull(numeros);
+        ArgumentOutOfRangeException.ThrowIfGreaterThan(numeros.Length, 100, nameof(numeros));
+        if (numeros.Length == 0)
+            throw new ArgumentException("Числа не предоставлены", nameof(numeros));
+
+        int firstMinimal = int.MaxValue;
+        int secondMinimal = int.MaxValue;
+
+        int firstIndex = int.MaxValue;
+        int secondIndex = int.MinValue;
+
+        Console.WriteLine("Тек. индекс\tИндекс числа");
+        for (int i = 0; i < numeros.Length; i++)
+        {
+            int secondLook = Math.Clamp(numeros.Length - i - 1, 0, numeros.Length - 1);
+            if (IsLesser(secondMinimal, numeros[secondLook]) && secondIndex != firstIndex)
+            {
+                int secondBuffer = numeros[secondLook];
+                secondIndex = secondLook;
+                secondMinimal = secondBuffer;
+            }
+
+            if (IsLesser(firstMinimal, numeros[i]) && firstIndex != secondIndex && i != secondIndex)
+            {
+                int firstBuffer = numeros[i];
+                firstIndex = i; 
+                firstMinimal = firstBuffer;
+            }
+            Console.Write($"{i}/{secondLook} \t\t {firstIndex}/{secondIndex}\n");
+        }
+
+        Console.WriteLine($"\nПервое минимальное: {firstMinimal}\nВторое минимальное: {secondMinimal}");
+
+        int sum = firstMinimal + secondMinimal;
+        Console.WriteLine($"Сумма минимальных: {sum}");
+        return sum;
+    }
+
+    static bool IsLesser(int max, int min)
+    {
+        if (max > min)
+            return true;
+        return false;
+    }
+}
---
+++ TestDoubleMinimal/Test1.cs
@@ -0,0 +1,101 @@
+﻿namespace TestDoubleMinimal
+{
+    [TestClass]
+    public sealed class DoubleMinimalTests
+    {
+        [TestMethod]
+        public void TestSum1()
+        {
+            int[] testNums = { 11, 22, 33, -22 };
+            int expected = -11;
+            
+            int actual = Program.FindMinimalsSummirize(testNums);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestSum2()
+        {
+            int[] testNums = { 3, 1, 2, 1, 1, 2, 3, 2, 3, 4, 1 };
+            int expected = 2;
+
+            int actual = Program.FindMinimalsSummirize(testNums);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestSum3()
+        {
+            int[] testNums = { 4, 0, 3, 19, 492, -10, 1 };
+            int expected = -10;
+
+            int actual = Program.FindMinimalsSummirize(testNums);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestSum4()
+        {
+            int[] testNums = { 0, -1, 2, -2, 1 };
+            int expected = -3;
+
+            int actual = Program.FindMinimalsSummirize(testNums);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestSum5()
+        {
+            int[] testNums = { 30, 20, 10, 0, 10, 20, 30 };
+            int expected = 10;
+
+            int actual = Program.FindMinimalsSummirize(testNums);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestSum6()
+        {
+            int[] testNums = { -30, -20, -10, 0, -10, -20, -30 };
+            int expected = -60;
+
+            int actual = Program.FindMinimalsSummirize(testNums);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestSum7()
+        {
+            int[] testNums = { 30, 20, 10, 0, -10, -20, -30 };
+            int expected = -50;
+
+            int actual = Program.FindMinimalsSummirize(testNums);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestNull()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() => Program.FindMinimalsSummirize(null));
+        }
+
+        [TestMethod]
+        public void TestNoNums()
+        {
+            Assert.ThrowsException<ArgumentException>(() => Program.FindMinimalsSummirize(Array.Empty<int>()));            
+        }
+
+        [TestMethod]
+        public void TestOverflow()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => Program.FindMinimalsSummirize(new int[101]));
+        }
+    }
+}
